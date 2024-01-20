f64e9b005ba2ad8ed87162d321d9c5f9e8ae40de
Mastay <nasywanfadilah@gmail.com>
2024-01-20 12:42:47 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Error</div>
+  },
+  {
+    path: '/anime',
+    element: <div>Anime Page</div>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
